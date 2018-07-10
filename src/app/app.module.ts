87522c20263d874ca5468badd5de09c9f37974a7
Vineeta Sharma <vineetasharma18791@gmail.com>
2018-07-10 16:39:07 +0530

    Basic routing configurations added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoComponent } from './todo/todo.component';
 import { TodoEditComponent } from './todo-edit/todo-edit.component';
 
+
+const appRoutes: Routes = [
+  { path: 'edit-todo', component: TodoEditComponent },
+  { path: '**', component: TodoListComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,6 +22,10 @@ import { TodoEditComponent } from './todo-edit/todo-edit.component';
     TodoEditComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     FormsModule
   ],
